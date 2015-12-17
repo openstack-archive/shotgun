5467df453679b9fd183d2b136d8a76ac2d581812
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2015-12-17 17:05:21 +0300

    use python-cliff for CLI
    Shotgun now uses python-cliff to provide
    CLI like:
    
    shotgun2 snapshot --input-file /path/to/snopshot_config.yaml
    
    This change will allow us to add other subcommands
    like "shotgun2 report" that is to be used for gathering
    short report about the master node (instead of version.yaml)
    
    Previous version of shotgun CLI is still available
    and deprecated.
    
    Change-Id: I3bc1f711d0e0fcf06f9414df1533d641a0e46595
    Related-Bug: #1515517

---
+++ shotgun/cli2.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import logging
+import yaml
+
+from cliff.app import App
+from cliff.command import Command
+from cliff.commandmanager import CommandManager
+
+from shotgun.logger import configure_logger
+
+from shotgun.config import Config
+from shotgun.manager import Manager
+
+
+logger = logging.getLogger(__name__)
+
+
+class Base(object):
+    def initialize_cmd(self, parsed_args):
+        with open(parsed_args.config, "r") as f:
+            self.config = Config(yaml.safe_load(f))
+        self.manager = Manager(self.config)
+
+
+class SnapshotCommand(Command, Base):
+
+    def get_parser(self, prog_name):
+        parser = super(SnapshotCommand, self).get_parser(prog_name)
+        parser.add_argument(
+            '--config',
+            required=True,
+            help='Path to snapshot config file')
+        return parser
+
+    def take_action(self, parsed_args):
+        """Generates snapshot
+
+        :param args: argparse object
+        """
+        self.initialize_cmd(parsed_args)
+        snapshot_path = self.manager.snapshot()
+        logger.info(u'Snapshot path: {0}'.format(snapshot_path))
+
+
+def main(argv=None):
+    configure_logger()
+    return App(
+        description="Shotgun CLI",
+        version='v2',
+        command_manager=CommandManager('shotgun')
+    ).run(argv)
