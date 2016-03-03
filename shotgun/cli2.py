88ae3112fd3cee9f8074ed063e9dcd082527e77c
Volodymyr Shypyguzov <vshypyguzov@mirantis.com>
2016-03-03 16:03:29 +0200

    Change output to one column, add number of lines arg, fix logging
    1. Change output format to one column
    2. Fix logging. Move all loggers to instance attributes to get use of
       builtin cliff commands -v, -q
    3. Add 'lines' positional parameter that specifies how many lines of
       package info to show. Default value is 3.
    4. Fix manager unit tests
    5. Moved 'add one line after changelog' logic to python code. Now it
       shows blank line after changelog if number of lines to show is > 1
    
    Example usage:
        shotgun2 report   - Shows 3 lines per package
        shotgun2 report 1 - Shows 1 line per package
    
    Change-Id: I465eca3c509196d6bdc850d0b73fed1e5fb3a3d7
    Closes-Bug: #1551762

--- shotgun/cli2.py
+++ shotgun/cli2.py
@@ -13,25 +13,22 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-
-import logging
-import yaml
-
 from cliff.app import App
 from cliff.command import Command
 from cliff.commandmanager import CommandManager
 from cliff.lister import Lister
 
-import shotgun
 from shotgun.config import Config
-from shotgun.logger import configure_logger
 from shotgun.manager import Manager
 
-
-logger = logging.getLogger(__name__)
+import logging
+import shotgun
+import yaml
 
 
 class Base(object):
+    log = logging.getLogger(__name__)
+
     def initialize_cmd(self, parsed_args):
         with open(parsed_args.config, "r") as f:
             self.config = Config(yaml.safe_load(f))
@@ -55,20 +52,20 @@ class SnapshotCommand(Command, Base):
         """
         self.initialize_cmd(parsed_args)
         snapshot_path = self.manager.snapshot()
-        logger.info(u'Snapshot path: {0}'.format(snapshot_path))
+        self.log.info(u'Snapshot path: {0}'.format(snapshot_path))
 
     def run(self, parsed_args):
         """Overriden for returning errno from exceptions"""
         try:
             return super(SnapshotCommand, self).run(parsed_args)
         except Exception as err:
-            logger.error(err)
+            self.log.error(err)
             return getattr(err, 'errno', 1)
 
 
 class ReportCommand(Lister, Base):
-
-    columns = ['Host', 'Reporter', 'Report']
+    columns = ['Info']
+    formatter_default = 'value'
 
     def get_parser(self, prog_name):
         parser = super(ReportCommand, self).get_parser(prog_name)
@@ -76,16 +73,22 @@ class ReportCommand(Lister, Base):
             '--config',
             default='/etc/shotgun/report.yaml',
             help='Path to report config file')
+        parser.add_argument(
+            'lines',
+            action='store',
+            nargs='?',
+            default=1,
+            help='Package info lines to show. If not set, default=1 is used')
         return parser
 
     def take_action(self, parsed_args):
         self.initialize_cmd(parsed_args)
-        data = [line for line in self.manager.report()]
-        return (self.columns, data)
+        raw = self.manager.report(parsed_args)
+        data = [line[2:3] for line in raw]
+        return self.columns, data
 
 
 def main(argv=None):
-    configure_logger()
     return App(
         description="Shotgun CLI",
         version=shotgun.__version__,
