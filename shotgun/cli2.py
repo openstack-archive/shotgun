0901bb41a6292c02df4c27ec41653e048ca491e7
Volodymyr Shypyguzov <vshypyguzov@mirantis.com>
2016-03-03 16:03:29 +0200

    Change output to one column, add option for number of lines to show
    Add 'lines' positional parameter that specifies how many lines of
    package info to show. Default value is 3.
    
    Example usage:
        shotgun2 report   - Shows 3 lines per package
        shotgun2 report 1 - Shows 1 line per package
    
    Change-Id: I465eca3c509196d6bdc850d0b73fed1e5fb3a3d7
    Closes-Bug: #1551762

--- shotgun/cli2.py
+++ shotgun/cli2.py
@@ -68,7 +68,7 @@ class SnapshotCommand(Command, Base):
 
 class ReportCommand(Lister, Base):
 
-    columns = ['Host', 'Reporter', 'Report']
+    columns = ['']
 
     def get_parser(self, prog_name):
         parser = super(ReportCommand, self).get_parser(prog_name)
@@ -76,12 +76,19 @@ class ReportCommand(Lister, Base):
             '--config',
             default='/etc/shotgun/report.yaml',
             help='Path to report config file')
+        parser.add_argument(
+            'lines',
+            action='store',
+            nargs='?',
+            default=3,
+            help='Package info lines to show. If not set, default=3 is used')
         return parser
 
     def take_action(self, parsed_args):
         self.initialize_cmd(parsed_args)
-        data = [line for line in self.manager.report()]
-        return (self.columns, data)
+        raw = self.manager.report(parsed_args)
+        data = [line[2:3] for line in raw]
+        return self.columns, data
 
 
 def main(argv=None):
